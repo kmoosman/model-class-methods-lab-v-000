3904f861d2327dcf781944e554c9186c00fe622d
Katie Moosman <moosman.katie@gmail.com>
2019-04-22 15:21:46 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,8 @@
 class Boat < ActiveRecord::Base
+  include ActiveRecord::QueryMethods
+
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
 end
