ba8d34171036f95ac3bb88e67efa710c279d15f2
Katie Moosman <moosman.katie@gmail.com>
2019-04-22 15:22:29 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
-  
+
    def self.first_five
      limit(5)
    end
